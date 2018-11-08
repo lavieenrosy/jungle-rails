4ab94495f1032093b5eebd3b07f6f67d72178e46
Rosy Lee <rosyhlee@gmail.com>
2018-11-08 21:17:00 +0000

    added sessions controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   get '/signup', to: 'users#new'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   resources :orders, only: [:create, :show]
 
   namespace :admin do
