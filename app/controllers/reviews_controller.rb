ffe7bb96c3bade8530c551185b1122f38c7c1e5c
Rosy Lee <rosyhlee@gmail.com>
2018-11-10 03:29:25 +0000

    completed product review feature

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+
+  def create
+    parent = Product.find(params[:product_id])
+    user_id = User.find(session[:user_id]).id if session[:user_id]
+    review1 = parent.reviews.new(review_params)
+    review1.user_id = user_id
+
+    if !review1.save
+      puts "Error! Review failed to save."
+    end
+
+    redirect_to parent
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
+end
