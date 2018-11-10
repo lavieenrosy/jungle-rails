8e9bb9b55b309cbdd02e92399f4307770afd1781
Rosy Lee <rosyhlee@gmail.com>
2018-11-10 04:02:18 +0000

    hid review form from unregistered users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,8 @@ class ReviewsController < ApplicationController
     user_id = User.find(session[:user_id]).id if session[:user_id]
     review1 = parent.reviews.new(review_params)
     review1.user_id = user_id
+    # @review.user = current_user
+    # puts @review.user
 
     if !review1.save
       puts "Error! Review failed to save."
